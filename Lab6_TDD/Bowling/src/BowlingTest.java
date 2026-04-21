e3e6552a0721a951b9e8bfb6e9d475a6f2aaf2f0
Hlib Bokatov <258564@edu.p.lodz.pl>
2026-04-21 17:40:46 +0200

    Add a first 0 roll test

--- Lab6_TDD/Bowling/src/BowlingTest.java
+++ Lab6_TDD/Bowling/src/BowlingTest.java
@@ -1,6 +1,14 @@
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BowlingTest {
-    Game bowl1 = new Game();
-
+    @Test
+    public void testGutterGame() {
+        Game g = new Game();
+        for (int i = 0; i < 20; i++){
+            g.roll(0);
+        }
+        assertEquals(0, g.score());
+    }
 }
