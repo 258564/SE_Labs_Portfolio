c572bb01490151f91b6ea346d71aa7b784159b51
Hlib Bokatov <258564@edu.p.lodz.pl>
2026-04-26 14:58:11 +0200

    red: initial state is NS_GREEN

---
+++ Project/Traffic_Lights_System/src/TrafficLightControllerTest.java
@@ -0,0 +1,18 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TrafficLightControllerTest {
+
+    private TrafficLightController controller;
+
+    @Before
+    public void setUp() {
+        controller = new TrafficLightController();
+    }
+
+    @Test
+    public void testInitialStateIsNorthSouthGreen() {
+        assertEquals(TrafficLightController.State.NS_GREEN, controller.getState());
+    }
+}
